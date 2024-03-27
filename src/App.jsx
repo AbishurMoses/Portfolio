21f9204506dabbeec4621793a7deb3a39c90401a
Abishur Moses <abishur@gmail.com>
2024-03-27 17:54:49 -0400

    Combined all projects

--- src/App.jsx
+++ src/App.jsx
@@ -49,20 +49,16 @@ function App() {
           <section id="about-me">
             <div id="wrapper_p">
               <div id="image-me">Image of me goes here</div>
-              <p>Hi! I'm Abishur Moses. Ever since I was born at a young age, I always tried to find answers to
-                problems and problems to answers. Before long, I realized that it was too much work. So,
-                I decided to learn about computers to help me in my endeavor.
-                I'm currently a junior, earning my bachelor's in computer science at Southern Adventist University.
-                During my sophomore years I dipped my toes in the Software industry by interning at Symvara.
-                I've managed to learn to work in a diverse team, follow a workflow and last but not least, solve real-world problems using code.
-                If you ever see me playing any racket-based sport or hiking on Saturdays, don't hesitate to start a chat.
+              <p>Hi! I'm Abishur Moses, a Computer Science student eager to leverage technology to create lasting solutions.
+                I have been able to cultivate my skills by working with a team of dedicated developers and engineers to create robust software.
+                When my laptop and Starbucks are closed, you can find me playing badminton, ping pong or almost any sport that involves a racket.
               </p>
             </div>
           </section>
+          <div id="experience-title">
+            <p id="pro-title">Projects</p>
+          </div>
           <section id="experience">
-            <div id="experience-title">
-              <p id="pro-title">Professional Projects <span>👉</span></p>
-            </div>
             <div id="jobs">
               <div id="symvara" >
                 <div id="sym-title-con">
@@ -71,31 +67,29 @@ function App() {
                 </div>
                 <div id="sym-des-cont">
                   <p id="sym-des">
-                    Currently working with a team of software engineers to create a comprehensive college housing management software.
-                    The software will enable clients to manage leave tracking, security checks, housing structure, among other
+                    Currently working with a team of software engineers to create a comprehensive college housing management software application.
+                    This application will enable clients to manage leave tracking, security checks, housing structure, among other
                     functionalities tailored  to meet the specific needs of a college housing management.
                   </p>
                 </div>
                 <div id="sym-details-cont">
                   <ul id="sym-details">
-                    <li> Developing a backend application using <b>NESTJS</b> with <b>MikroORM</b>, <b>MySQL</b> and a Git/Github workflow. </li>
+                    <li> Developing a backend application using <b>NestJS</b> with <b>MikroORM</b>, <b>MySQL</b> and a Git/Github workflow. </li>
                     <li> Implemented automatic schema updates using <b>MikroORM</b> for seamless database management. </li>
-                    <li> Created 4 automated testing suites with 200+ individual tests with <b>Jest</b>. </li>
+                    <li> Created 4 automated testing suites with 200+ individual tests using <b>Jest</b>. </li>
                     <li> Wrote documentation outlining automated testing procedures and modified existing database documentation. </li>
                   </ul>
                 </div>
                 <div id="sym-lan-cont">
-                  <p id="sym-lan">Utilized: TypeScript, NestJS, MySQL, Jest, Git, Github, Jira</p>
+                  <p id="sym-lan"><span>Utilized:</span> TypeScript, NestJS, MySQL, Jest, Git, Github, Jira</p>
                 </div>
               </div>
             </div>
           </section>
+          {/* <p id="personal-pro-tag"> Personal Projects</p>
           <section id="projects">
             <div id="nuclear-sim" onClick={nuclear}>
-              {/* <div id="nuclear-tip" onClick={nuclear}>
-                <p>Nuclear Power Plant Sim</p>
-              </div> */}
-                <p>Nuclear Power Plant Sim</p>
+              <p>Nuclear Power Plant Sim</p>
             </div>
             <div class="todo-projects">
               <p>Coding in Process!</p>
@@ -103,7 +97,7 @@ function App() {
             <div class="todo-projects">
               <p>Coding in Process!</p>
             </div>
-          </section>
+          </section> */}
         </section>
       </main >
     </div >
